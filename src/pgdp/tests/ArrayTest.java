29ba92eb0ede2d3b71449e2733c4ec34c19374b9
Said Tamim Sadat <ge87mit@mytum.de>
2022-11-11 19:12:39 +0100

    test2

--- src/pgdp/tests/ArrayTest.java
+++ src/pgdp/tests/ArrayTest.java
@@ -22,14 +22,12 @@ public class ArrayTest {
 		int[] a = new int[] {};
 		print(a);
 		Assert.assertEquals("{}", getConsoleOutput());
-
-		int[] b = new int[] {Integer.MAX_VALUE, Integer.MIN_VALUE};
+	}
+	@Test
+	void testPrintminmax() {
+		int[] b = new int[]{Integer.MAX_VALUE, Integer.MIN_VALUE};
 		print(b);
 		Assert.assertEquals("{2147483647, " + "-2147483648}", getConsoleOutput());
 		PinguLib.reset();
 	}
-
-
-
-
 }
